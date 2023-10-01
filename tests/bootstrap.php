f7189334921ce64b91eb8d8e5173fb488c651239
Roman Kříž <roman@kriz.io>
2023-10-01 12:50:47 +0200

    ci: try to use WC's bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,22 +23,19 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require MIGUEL_PROJECT_DIR . '/woocommerce/plugins/woocommerce/woocommerce.php';
-	require MIGUEL_PROJECT_DIR . '/miguel.php';
+	require_once MIGUEL_PROJECT_DIR . '/woocommerce/plugins/woocommerce/woocommerce.php';
+	require_once MIGUEL_PROJECT_DIR . '/miguel.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Plugin helpers
-require MIGUEL_PROJECT_DIR . '/tests/helpers/class-miguel-helper-http.php';
-require MIGUEL_PROJECT_DIR . '/tests/helpers/class-miguel-helper-order.php';
-require MIGUEL_PROJECT_DIR . '/tests/helpers/class-miguel-helper-product.php';
+require_once MIGUEL_PROJECT_DIR . '/tests/helpers/class-miguel-helper-http.php';
+require_once MIGUEL_PROJECT_DIR . '/tests/helpers/class-miguel-helper-order.php';
+require_once MIGUEL_PROJECT_DIR . '/tests/helpers/class-miguel-helper-product.php';
 
 // WooCommerce helpers
-$wc_tests_framework_base_dir = MIGUEL_PROJECT_DIR . '/woocommerce/plugins/woocommerce/tests/legacy/framework/';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-shipping.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-product.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-order.php';
+require_once MIGUEL_PROJECT_DIR . '/woocommerce/plugins/woocommerce/tests/legacy/bootstrap.php';
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require_once $_tests_dir . '/includes/bootstrap.php';
