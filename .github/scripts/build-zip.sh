f543689a1f2bafa7175d0374c66b9a1025cb85f3
Roman Kříž <roman@kriz.io>
2023-10-05 17:13:16 +0200

    chore: convert changelog to WooCommerce version

--- .github/scripts/build-zip.sh
+++ .github/scripts/build-zip.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 tmpdir=$(mktemp -d)
 result="$(pwd)/miguel.zip"
 
 rm -rf "$result"
 
 mkdir -p $tmpdir/miguel
-rsync -a --exclude=.git --exclude=run --exclude=vendor assets includes languages CHANGELOG.md README.md LICENSE miguel.php "${tmpdir}/miguel/"
+rsync -a --exclude=.git --exclude=run --exclude=vendor assets includes languages changelog.txt README.md LICENSE miguel.php "${tmpdir}/miguel/"
 
 pushd $tmpdir > /dev/null
     find miguel -name ".DS_Store" -depth -exec rm {} \;
